f88b80e78772a7735f521395169428ae5d5327b9
ArtZ23 <zzartur77@gmail.com>
2022-11-30 11:39:06 +0400

    Visual studio changes

---
+++ Backend_week3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Backend_week3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("test");
+
+			Console.WriteLine("   /|");
+			Console.WriteLine("  / |");
+			Console.WriteLine(" /  |");
+			Console.WriteLine("/_ _|");
+
+			Console.BackgroundColor = ConsoleColor.Green;
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("random text");
+			Console.BackgroundColor = ConsoleColor.Blue;
+		}
+	}
+}
